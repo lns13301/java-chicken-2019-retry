97a127946732c8306c8e59564ef5b893c0f674a6
lns13301 <rkdrbgks22@ks.ac.kr>
2019-12-23 22:49:07 +0900

    [ADD] Menu 목록을 보여주고 입력받는 부분

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -5,8 +5,16 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static int inputTableNumber() {
+    public static String inputTableNumber() {
         System.out.println("## 주문할 테이블을 선택하세요.");
-        return scanner.nextInt();
+        return scanner.nextLine();
+    }
+
+    public static String printMenu() {
+        System.out.println("## 메인화면");
+        System.out.println("1 - 주문등록");
+        System.out.println("2 - 결제하기");
+        System.out.println("3 - 프로그램 종료");
+        return scanner.nextLine();
     }
 }
