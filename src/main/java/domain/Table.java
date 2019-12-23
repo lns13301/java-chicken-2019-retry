7eda5cf2f7ca70dedba423bedd178dad4141f7a3
lns13301 <rkdrbgks22@ks.ac.kr>
2019-12-23 23:02:04 +0900

    [ADD] 테이블에 착석, 미착석 상태를 확인하여 출력하는 부분

--- src/main/java/domain/Table.java
+++ src/main/java/domain/Table.java
@@ -2,6 +2,7 @@ package domain;
 
 public class Table {
     private final int number;
+    private boolean isSeatEmpty = true;
 
     public Table(final int number) {
         this.number = number;
@@ -10,5 +11,13 @@ public class Table {
     @Override
     public String toString() {
         return Integer.toString(number);
+    }
+
+    public boolean isSeatEmpty() {
+        return isSeatEmpty;
+    }
+
+    public void isSeatEmpty(boolean isSeatEmpty) {
+        this.isSeatEmpty = isSeatEmpty;
     }
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -2,6 +2,7 @@ package view;
 
 import domain.Menu;
 import domain.Table;
+import domain.TableRepository;
 
 import java.util.List;
 
@@ -9,6 +10,7 @@ public class OutputView {
     private static final String TOP_LINE = "┌ ─ ┐";
     private static final String TABLE_FORMAT = "| %s |";
     private static final String BOTTOM_LINE = "└ ─ ┘";
+    private static final String BOTTOM_LINE_SEAT = "└ ₩ ┘";
 
     public static void printTables(final List<Table> tables) {
         System.out.println("## 테이블 목록");
@@ -26,7 +28,7 @@ public class OutputView {
 
     private static void printLine(final String line, final int count) {
         for (int index = 0; index < count; index++) {
-            System.out.print(line);
+            System.out.print(printSeatYesOrNo(TableRepository.tables(), index));
         }
         System.out.println();
     }
@@ -36,5 +38,12 @@ public class OutputView {
             System.out.printf(TABLE_FORMAT, table);
         }
         System.out.println();
+    }
+
+    private static String printSeatYesOrNo(List<Table> table, int index) {
+        if (table.get(index).isSeatEmpty()) {
+            return BOTTOM_LINE;
+        }
+        return BOTTOM_LINE_SEAT;
     }
 }
