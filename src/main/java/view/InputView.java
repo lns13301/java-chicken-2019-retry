48f927883d13f27ce43055b85bead0308c9e7527
DB <rkdrbgks22@ks.ac.kr>
2019-12-24 16:54:50 +0900

    [UPDATE] 문장마다 New Line 으로 시작하도록 함

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,11 +6,12 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InputView {
+    private static final String NEW_LINE = "\n";
     private static final Scanner scanner = new Scanner(System.in);
     private static Validator validator = new Validator();
 
     public static int inputTableNumber() {
-        System.out.println("## 주문할 테이블을 선택하세요.");
+        System.out.println(NEW_LINE + "## 주문할 테이블을 선택하세요.");
         String input = scanner.nextLine();
 
         validator.isNull(input);
@@ -18,14 +19,14 @@ public class InputView {
             int value = Integer.parseInt(input);
             return value;
         } catch (NumberFormatException e) {
-            System.out.println("숫자를 입력해야 합니다.");
+            System.out.println(NEW_LINE + "숫자를 입력해야 합니다.");
             return inputTableNumber();
         }
     }
 
     public static int inputMain() {
         OutputView.printMain();
-        System.out.println("## 원하는 기능을 선택하세요.");
+        System.out.println(NEW_LINE + "## 원하는 기능을 선택하세요.");
         String input = scanner.nextLine();
 
         validator.isNull(input);
@@ -33,13 +34,13 @@ public class InputView {
             int value = Integer.parseInt(input);
             return value;
         } catch (NumberFormatException e) {
-            System.out.println("숫자를 입력해야 합니다.");
+            System.out.println(NEW_LINE + "숫자를 입력해야 합니다.");
             return inputMain();
         }
     }
 
     public static int inputMenu() {
-        System.out.println("## 등록할 메뉴를 선택하세요.");
+        System.out.println(NEW_LINE + "## 등록할 메뉴를 선택하세요.");
         String input = scanner.nextLine();
 
         validator.isNull(input);
@@ -47,13 +48,13 @@ public class InputView {
             int value = Integer.parseInt(input);
             return value;
         } catch (NumberFormatException e) {
-            System.out.println("숫자를 입력해야 합니다.");
+            System.out.println(NEW_LINE + "숫자를 입력해야 합니다.");
             return inputMenu();
         }
     }
 
     public static int inputCount() {
-        System.out.println("## 메뉴의 수량을 입력하세요.");
+        System.out.println(NEW_LINE + "## 메뉴의 수량을 입력하세요.");
         String input = scanner.nextLine();
 
         validator.isNull(input);
@@ -61,7 +62,7 @@ public class InputView {
             int value = Integer.parseInt(input);
             return value;
         } catch (NumberFormatException e) {
-            System.out.println("숫자를 입력해야 합니다.");
+            System.out.println(NEW_LINE + "숫자를 입력해야 합니다.");
             return inputCount();
         }
     }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -7,13 +7,14 @@ import domain.TableRepository;
 import java.util.List;
 
 public class OutputView {
+    private static final String NEW_LINE = "\n";
     private static final String TOP_LINE = "┌ ─ ┐";
     private static final String TABLE_FORMAT = "| %s |";
     private static final String BOTTOM_LINE = "└ ─ ┘";
     private static final String BOTTOM_LINE_SEAT = "└ ₩ ┘";
 
     public static void printTables(final List<Table> tables) {
-        System.out.println("## 테이블 목록");
+        System.out.println(NEW_LINE + "## 테이블 목록");
         final int size = tables.size();
         printTopLine(TOP_LINE, size);
         printTableNumbers(tables);
@@ -55,7 +56,7 @@ public class OutputView {
     }
 
     static void printMain() {
-        System.out.println("## 메인화면");
+        System.out.println(NEW_LINE + "## 메인화면");
         System.out.println("1 - 주문등록");
         System.out.println("2 - 결제하기");
         System.out.println("3 - 프로그램 종료");
