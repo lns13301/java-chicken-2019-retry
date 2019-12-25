9e53a470890d55cc1ce3cede053d06a482fc7f3e
lns13301 <rkdrbgks22@ks.ac.kr>
2019-12-26 01:48:52 +0900

    [ADD] 치킨을 10개 단위로 구매 시 할인하는 기능
    - 치킨인지 아닌지 분류하는 기능
    - 치킨 수량을 파악하여 할인 금액을 반환하는 기능

--- src/main/java/domain/BillManager.java
+++ src/main/java/domain/BillManager.java
@@ -1,28 +1,45 @@
 package domain;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class BillManager {
     private static final int EMPTY = 0;
+    private static final int CARD = 1;
+    private static final int CASH = 2;
+    private static final int BULK = 10;
+    private static final int DISCOUNT_AMOUNT = 10000;
     private static final String DEFAULT = "";
 
     public static Map<Integer, List<Menu>> bill(List<Menu> menus) {
         return menus.stream().collect(Collectors.groupingBy(Menu::getNumber));
     }
 
-    public static int totalPayment(Map<Integer, List<Menu>> bills) {
+    public static int totalPayment(Map<Integer, List<Menu>> bills, int paymentMethod) {
         int toPay = EMPTY;
 
         while (!bills.isEmpty()) {
             int key = distinctMenu(bills);
-            toPay += bills.get(key).stream().mapToInt(menu -> menu.getPrice()).reduce((a, b) -> a + b).getAsInt();
+            toPay += bills.get(key).stream().mapToInt(Menu::getPrice).reduce(Integer::sum).getAsInt();
+            toPay -= chickenDiscount(bills.get(key));
             bills.remove(key);
         }
 
         return toPay;
+    }
+
+    private static int chickenDiscount(List<Menu> menus) {
+        if (isChicken(menus)) {
+            return menus.size() / BULK * DISCOUNT_AMOUNT;
+        }
+        return EMPTY;
+    }
+
+    private static boolean isChicken(List<Menu> menus) {
+        return menus.stream().anyMatch(menu -> menu.getCategory() == Category.CHICKEN);
     }
 
     private static int distinctMenu(Map<Integer, List<Menu>> bills) {
